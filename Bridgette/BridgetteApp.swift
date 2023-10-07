119f14fab698cf4b38a90b2dc4d862718aeefdd7
DavidEtoedia <etoedia@gmail.com>
2023-10-07 11:12:40 -0700

    Initial Commit

---
+++ Bridgette/BridgetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BridgetteApp.swift
+//  Bridgette
+//
+//  Created by Inyene Etoedia on 07/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BridgetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
